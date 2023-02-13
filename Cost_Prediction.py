e056aea9ab5e57f34c1958030520fdfe1a2e0445
yodialfa <yodialfariz@gmail.com>
2023-02-13 13:18:24 +0700

    update

--- Cost_Prediction.py
+++ Cost_Prediction.py
@@ -5,7 +5,7 @@ import numpy as np
 import joblib
 # import math
 # from predict_cost import predict
-from sklearn.ensemble import RandomForestRegressor as rf
+from xgboost import XGBRegressor
 from streamlit_option_menu import option_menu
 import base64
 from PIL import Image
